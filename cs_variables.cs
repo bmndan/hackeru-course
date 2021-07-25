b9644e0e52545e64586313f833fe8842f1d4472b
Dan <bmndan@gmail.com>
2021-07-26 02:48:09 +0300

    Solution

--- cs_variables.cs
+++ cs_variables.cs
@@ -1,5 +1,6 @@
 using System;
 					
+// Q1
 public class GetBirthDate
 {
 	public static void Main()
@@ -18,14 +19,35 @@ public class GetBirthDate
 		Console.WriteLine(iDay + "." + iMonth + "." + iYear);
 		Console.WriteLine(iDay + "-" + iMonth + "-" + iYear);
 		Console.WriteLine(iDay + "/" + iMonth + "/" + iYear);
-		
 	}
 }
 
+// Q2
 public class LikePizza
 {
 	public static void Main()
 	{
-
+		string myName = "Daniel";
+		int myAge = 29;
+		bool likePizza = true;
+		double piNumber = 3.14;
+		Console.WriteLine("My program is running");
 	}
+}
+
+// Q3
+public class GetOrder
+{
+	public static void Main()
+	{
+		string product;
+		Console.WriteLine("# HackerU Shop #");
+		Console.WriteLine("Which products would you like to buy?");
+		Console.WriteLine("Milk");
+		Console.WriteLine("Chocolate");
+		Console.WriteLine("Apple");
+		product = Console.ReadLine();
+		System.Console.WriteLine("Thank you for buying {0}", product);
+	}
+
 }
